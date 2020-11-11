bd0db6be97b9632995a2751abb7d87b9100a62df
crystal  villanueva <noelani.villanueva@gmail.com>
2020-11-11 22:16:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@ def reverse_each_word(string)
 end
 reverse_each_word("Hello there, and how are you?")
 
-def collect(string)
+def reverse_each_word(string)
   array = string.split(" ")
   array.collect do |word|
     word.reverse!
